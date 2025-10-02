c94d07bfbaccb6d1c202a7fe45c54bfcc318913b
lhourquin <lhourquin@gmail.com>
2025-10-02 15:17:25 +0200

    init: Initialize project with essential files including configuration for ESLint, TypeScript, and Vite. Add .gitignore, package.json, and basic HTML structure. Set up initial styles and assets for the application.

---
+++ src/utils/constants.ts
@@ -0,0 +1,3 @@
+export const BASE_URL = import.meta.env.VITE_OMDB_BASE_URL;
+export const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
+export const BASE_URL_POSTER = import.meta.env.VITE_POSTER_OMDB_BASE_URL;
